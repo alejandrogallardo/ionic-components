5250c175c09bdee78606b6abb1c634afa4b37dcb
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-10-17 15:37:54 -0600

    App de componentes terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from './components/components.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import { PipesModule } from './pipes/pipes.module';
+
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ComponentsModule,
+    PipesModule,
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/pages/slides/slides.page.spec.ts
+++ src/app/pages/slides/slides.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { SlidesPage } from './slides.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('SlidesPage', () => {
+  let component: SlidesPage;
+  let fixture: ComponentFixture<SlidesPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ SlidesPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(SlidesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
